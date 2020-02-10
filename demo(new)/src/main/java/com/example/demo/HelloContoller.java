9252a47f3ab127e521381423616ac81ef8956d74
Canon <Canon@DESKTOP-PL2F7PK>
2020-02-10 14:55:50 +0900

    1

---
+++ demo(new)/src/main/java/com/example/demo/HelloContoller.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloContoller {
+	
+	@RequestMapping("/hello")
+	public String hello(Model m , String regid , String msg) {
+		System.out.println("regid :"+regid);
+		System.out.println("msg :"+msg);
+		m.addAttribute("msg","Hello");
+		return "hello";
+	}
+}
